47149e9e81c184de5804bb082f2a1a8155b6270b
BVK Ratnesh <ratneshbvk2504@gmail.com>
2025-12-29 19:15:52 +0530

    Adding dotenv for safety

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 import { registerRoomHandlers } from "./socket/roomHandlers.js";
+import { config } from "dotenv";
 
+config();
 const app = express();
 app.use(cors());
+
+// Health check
+app.get("/", (req, res) => {
+  res.send("Typing Race backend is running 🚀");
+});
 
 const server = http.createServer(app);
 
@@ -22,7 +29,7 @@ io.on("connection", (socket) => {
   registerRoomHandlers(io, socket);
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
